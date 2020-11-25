dff34db54c87f0f3db7dee7e5815ee465ca74b69
Gianpaolo Raimondi <gianpaolo.raimondi@gmail.com>
2020-11-25 16:15:08 +0100

    created website structure

---
+++ pages/MyTravelBook.js
@@ -0,0 +1,11 @@
+import Navbar from './Navbar'
+
+function MyTravelBook() {
+    return(
+    <div>
+        <Navbar />
+        <h1>Ciao, sono il tuo diario di viaggio</h1>
+    </div>)
+}
+
+export default MyTravelBook
