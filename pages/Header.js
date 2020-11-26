d9d2e1c8ed1d03ea086b52f782dde62dc0f5c285
Valerio Moscatelli <valerio.15.vm@gmail.com>
2020-11-26 12:40:59 +0100

    Updated header

--- pages/Header.js
+++ pages/Header.js
@@ -1,10 +1,11 @@
-import 'bootstrap/dist/css/bootstrap.min.css';
 import Image from 'react-bootstrap/Image'
 import styles from '../styles/header.module.css'
 import Container from 'react-bootstrap/Container'
 import Row from 'react-bootstrap/Row'
 import Col from 'react-bootstrap/Col'
 import Button from 'react-bootstrap/Button'
+import Link from 'next/link'
+import NewTrip from './NewTrip'
 
 function Header() {
     return (
@@ -22,12 +23,12 @@ function Header() {
             <div className={styles.Description}>
                 <h1 className={styles.ProfileName}> Cristina Munuz </h1>
                 <p className={styles.MyStory}> bifhriuvherjfnekiurhfieurhfkjenrfikvjehri uhfeiurhvierjgnkejrgv efugrhfierugbve riufbeirjb ifeirfubweif
-                wefcb9wi4urfbie w9eufhi3u4fhbiwue
+                                               wefcb9wi4urfbie w9eufhi3u4fhbiwue
                 </p>
             </div>
             <>
 
-                <Button className={styles.BottonToTrip} variant="success"> PLAN YOUR NEXT TRIP </Button>{' '}
+                <Button className={styles.BottonToTrip} variant="success" onClick> <Link href='./NewTrip'> PLAN YOUR NEXT TRIP </Link> </Button>{''}
 
             </>
         </div>
