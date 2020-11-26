a295e73272f046e8753206646ecaf91bf4252cff
Gianpaolo Raimondi <gianpaolo.raimondi@gmail.com>
2020-11-26 18:04:32 +0100

    added array mapping

--- pages/MyPlannedTrips.js
+++ pages/MyPlannedTrips.js
@@ -5,8 +5,51 @@ import ButtonGroup from "react-bootstrap/ButtonGroup";
 import Card from "react-bootstrap/Card";
 import Header from './Header'
 import styles from '../styles/MyPlannedTrips.module.css'
+import { useState } from 'react'
 
 function MyPlannedTrips() {
+
+  const trip = {
+      id: "Tokyo",
+      location: "Tokyo",
+      photo: "/Tokyo.jpg"
+    }
+
+  const [value, setValue] = useState('')
+
+  function handleChange(event) {
+    setValue(event.target.value);
+  }
+
+  const [cardViaggio, setcardViaggio] = useState([trip])
+
+    function addCard() {
+      const cardList = cardViaggio.slice();
+      const newCardViaggio = trip;
+      cardList.push(newCardViaggio);
+      setcardViaggio(cardList);
+    }
+
+    function renderCard() {
+      return cardViaggio.map((cardViaggio, index) => {
+        const {location, photo} = cardViaggio
+        return( <Card className={styles.foto} key={index}>
+          <Card.Img variant="top" /><img src={photo} width="200px" overflow="hidden"></img>
+          <Card.Body>
+            <Card.Title>{location}</Card.Title>
+            <Card.Text>
+            </Card.Text>
+            <Button><Link href="./NewTrip">Cross</Link></Button>
+          </Card.Body>
+        </Card>
+        )
+      })
+    }
+    function chooseLocation() {
+       
+    }
+    console.log(value);
+
   return (
     <div>
       <TopNavBar />
@@ -22,7 +65,17 @@ function MyPlannedTrips() {
         </Button>
       </ButtonGroup>
 
+   
+
       <div className={styles.flexcontainer}>
+      <form id="input" onSubmit={chooseLocation}>
+        <label>choose location</label>
+        <input type="text" placeholder="la 1 la 2 o la 3" value={value} onChange={handleChange} />
+        <input type="submit" value="Viaggia!"></input>
+      </form>
+
+      <button onClick={addCard}>Aggiungi viaggio</button>
+    <div>{renderCard()}</div>
 
         <Card className={styles.foto}>
           <Card.Img variant="top" /><img src="/Tokyo.jpg" width="200px" overflow="hidden"></img>
@@ -57,8 +110,6 @@ function MyPlannedTrips() {
           </Card.Body>
         </Card>
       </div>
-
-
 
     </div>
   );
