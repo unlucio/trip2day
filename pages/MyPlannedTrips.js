fceb980fd141d8b1f168dc5b69013282b9fa3d6a
Matteo Spanu <spawnmj@gmail.com>
2020-11-26 10:50:10 +0100

    la mia pagina creata

--- pages/MyPlannedTrips.js
+++ pages/MyPlannedTrips.js
@@ -1,11 +1,31 @@
-import Navbar from './Navbar'
+import Link from 'next/link';
+import Navbar from "./Navbar";
+import Button from 'react-bootstrap/Button';
+import ButtonGroup from 'react-bootstrap/ButtonGroup';
+
 
 function MyPlannedTrips() {
-    return(
+  return (
     <div>
-        <Navbar />
-        <h1>Ciao, sono l'elenco dei tuoi viaggi pianificati</h1>
-    </div>
-)}
+      <Navbar />
+      <h1>Ciao, sono l'elenco dei tuoi viaggi pianificati</h1>
+      <ButtonGroup aria-label="Basic example">
+        <Button variant="secondary"><Link href='./MyTrips'>My Trips</Link></Button>
+        <Button variant="secondary">My Planned Trips</Button>
+      </ButtonGroup>
 
-export default MyPlannedTrips
+
+
+
+
+      
+    </div>
+
+
+    
+
+
+  );
+}
+
+export default MyPlannedTrips;
--- pages/MyTrips.js
+++ pages/MyTrips.js
@@ -1,10 +1,17 @@
-import Navbar from './Navbar'
+import Link from 'next/link'
+import Navbar from "./Navbar";
+import Button from 'react-bootstrap/Button';
+import ButtonGroup from 'react-bootstrap/ButtonGroup';
 
 function MyTrips() {
     return(
     <div>
         <h1>Ciao, sono l'elenco dei tuoi viaggi</h1>
+        <ButtonGroup aria-label="Basic example">
+        <Button variant="secondary">My Trips</Button>
+        <Button variant="secondary"><Link href='./MyPlannedTrips'>My Planned Trips</Link></Button>
+      </ButtonGroup>
     </div>)
-}
+
 
 export default MyTrips
