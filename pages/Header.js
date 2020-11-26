b320f1c8fb158a28b41208070d749edc6e9a7490
Valerio Moscatelli <valerio.15.vm@gmail.com>
2020-11-26 16:03:54 +0100

    Head fix

--- pages/Header.js
+++ pages/Header.js
@@ -4,26 +4,29 @@ import Link from 'next/link'
 
 function Header() {
     return (
+
         <div className={styles.contenitore}>
-            <Container>
-                <Row>
-                    <Col xs={6} md={4}>
-                        <Image className={styles.images} src="https://i.pinimg.com/originals/ac/b9/6b/acb96bd45aa8123f05757b16666cd45d.jpg" roundedCircle width="200px" />
-                    </Col>
-                </Row>
-            </Container>
+
+
+            <Image className={styles.images} src="https://i.pinimg.com/originals/ac/b9/6b/acb96bd45aa8123f05757b16666cd45d.jpg" roundedCircle width="200px" />
+
+
             <div className={styles.Description}>
                 <h1 className={styles.ProfileName}> Cristina Munuz </h1>
                 <p className={styles.MyStory}> bifhriuvherjfnekiurhfieurhfkjenrfikvjehri uhfeiurhvierjgnkejrgv efugrhfierugbve riufbeirjb ifeirfubweif
-                                               wefcb9wi4urfbie w9eufhi3u4fhbiwue
+                wefcb9wi4urfbie w9eufhi3u4fhbiwue
                 </p>
+
+                <>
+
+                    <Button className={styles.BottonToTrip} variant="success"> <Link href='./NewTrip'> PLAN YOUR NEXT TRIP </Link> </Button>{''}
+
+                </>
             </div>
-            <>
 
-                <Button className={styles.BottonToTrip} variant="success" onClick> <Link href='./NewTrip'> PLAN YOUR NEXT TRIP </Link> </Button>{''}
-
-            </>
         </div>
+
+
     )
 }
 
