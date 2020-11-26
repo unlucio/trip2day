a74f879ce2e386e99a09506b9bdf4c20ae539a2d
Matteo Spanu <spawnmj@gmail.com>
2020-11-26 12:46:21 +0100

    Update MyPlanned + pics

--- pages/MyTrips.js
+++ pages/MyTrips.js
@@ -1,17 +1,18 @@
-import Link from 'next/link'
-import Navbar from "./Navbar";
-import Button from 'react-bootstrap/Button';
-import ButtonGroup from 'react-bootstrap/ButtonGroup';
+import Link from "next/link";
+import Button from "react-bootstrap/Button";
+import ButtonGroup from "react-bootstrap/ButtonGroup";
 
 function MyTrips() {
-    return(
+  return (
     <div>
-        <h1>Ciao, sono l'elenco dei tuoi viaggi</h1>
-        <ButtonGroup aria-label="Basic example">
+      <h1>Ciao, sono l'elenco dei tuoi viaggi</h1>
+      <ButtonGroup aria-label="Basic example">
         <Button variant="secondary">My Trips</Button>
-        <Button variant="secondary"><Link href='./MyPlannedTrips'>My Planned Trips</Link></Button>
+        <Button variant="secondary">
+          <Link href="./MyPlannedTrips">My Planned Trips</Link>
+        </Button>
       </ButtonGroup>
-    </div>)
-
-
+    </div>
+  );
+}
 export default MyTrips
