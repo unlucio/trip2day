de0015f29c7d367e5f7fc8cbf77f9b34b7c38c58
Gianpaolo Raimondi <gianpaolo.raimondi@gmail.com>
2020-11-26 16:50:28 +0100

    fixed routes and page links

--- pages/MyPlannedTrips.js
+++ pages/MyPlannedTrips.js
@@ -9,51 +9,54 @@ import styles from '../styles/MyPlannedTrips.module.css'
 function MyPlannedTrips() {
   return (
     <div>
-       <TopNavBar />
-            <Header />
+      <TopNavBar />
+      <Header />
       <h1>Ciao, sono l'elenco dei tuoi viaggi pianificati</h1>
       <ButtonGroup aria-label="Basic example">
         <Button variant="secondary">
           <Link href="./MyTrips">My Trips</Link>
         </Button>
-        <Button variant="secondary">My Planned Trips</Button>
+        <Button variant="secondary">My Planned Trips </Button>
+        <Button variant="secondary">
+          <Link href="./MyPhotos">My Photos</Link>
+        </Button>
       </ButtonGroup>
 
-    <div className={styles.flexcontainer}>
+      <div className={styles.flexcontainer}>
 
-      <Card className={styles.foto}>
-        <Card.Img variant="top"/><img src="/Tokyo.jpg" width="200px"overflow="hidden"></img>
-        <Card.Body>
-          <Card.Title>Tokyo</Card.Title>
-          <Card.Text>
-        
-          </Card.Text>
-          <Button><Link href="./NewTrip">Cross</Link></Button>
-        </Card.Body>
-      </Card>
+        <Card className={styles.foto}>
+          <Card.Img variant="top" /><img src="/Tokyo.jpg" width="200px" overflow="hidden"></img>
+          <Card.Body>
+            <Card.Title>Tokyo</Card.Title>
+            <Card.Text>
 
-      <Card style={{ width: "18rem" }}>
-        <Card.Img variant="top"/><img src="/Parigi.jpg" width="200px"overflow="hidden"></img>
-        <Card.Body>
-          <Card.Title>Parigi</Card.Title>
-          <Card.Text>
-            
-          </Card.Text>
-          <Button variant="primary">Go somewhere</Button>
-        </Card.Body>
-      </Card>
+            </Card.Text>
+            <Button><Link href="./NewTrip">Cross</Link></Button>
+          </Card.Body>
+        </Card>
 
-      <Card style={{ width: "18rem" }}>
-        <Card.Img variant="top"/><img src="/Formentera.jpg" width="200px"overflow="hidden"></img>
-        <Card.Body>
-          <Card.Title>Formentera</Card.Title>
-          <Card.Text>
-            
-          </Card.Text>
-          <Button variant="primary">Go somewhere</Button>
-        </Card.Body>
-      </Card>
-    </div>
+        <Card style={{ width: "18rem" }}>
+          <Card.Img variant="top" /><img src="/Parigi.jpg" width="200px" overflow="hidden"></img>
+          <Card.Body>
+            <Card.Title>Parigi</Card.Title>
+            <Card.Text>
+
+            </Card.Text>
+            <Button variant="primary">Go somewhere</Button>
+          </Card.Body>
+        </Card>
+
+        <Card style={{ width: "18rem" }}>
+          <Card.Img variant="top" /><img src="/Formentera.jpg" width="200px" overflow="hidden"></img>
+          <Card.Body>
+            <Card.Title>Formentera</Card.Title>
+            <Card.Text>
+
+            </Card.Text>
+            <Button variant="primary">Go somewhere</Button>
+          </Card.Body>
+        </Card>
+      </div>
 
 
 
