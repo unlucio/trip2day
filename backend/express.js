484f543cffccb8a350c00482b6cb2006954c0389
Luciano Colosio <unlucio@gmail.com>
2020-11-30 18:32:14 +0100

    Adding databvase server support:
    - Added Generic Node Dockerfile
    - Added docker-compose file
    - Added sample app config for docker-compose
    - Added mysql server in docker-compose
    - Added phpmyadmin in docker-compose
    - Added table structure for `users` and `locations`

--- backend/express.js
+++ backend/express.js
@@ -2,7 +2,7 @@ const express = require("express");
 const cors = require("cors");
 const bodyParser = require('body-parser')
 const app = express();
-const port = 3001;
+const port = process.env.PORT || 3001;
 app.use(cors());
 
 const trip = [
